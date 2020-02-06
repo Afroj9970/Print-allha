047167ddc0446c6324dfe8df965a01dcf462bd1b
Afroj9970 <akshay9096kale@gmail.com>
2020-02-06 16:15:08 +0530

    Add code

---
+++ src/com/first/Demo.java
@@ -0,0 +1,16 @@
+package com.first;
+
+/**
+ * @author Afroz
+ *
+ */
+public class Demo {
+	
+	public static void main(String args[])
+	{
+		System.out.print("My Allha" +" Is Greater");
+	}
+	
+	String str;
+
+}
---
+++ src/com/first/TestNG.java
@@ -0,0 +1,11 @@
+package com.first;
+
+import org.testng.annotations.Test;
+
+public class TestNG {
+
+	@Test
+	public void first() {
+		System.out.println("Hi..");
+	}
+}
